4eaae98dc2646734be2fb5c810493a6cc0d37df5
莫育良 <moyl@signalway.com.cn>
2020-10-13 10:54:57 +0800

    2020年10月13日10:54:52

--- Student.py
+++ Student.py
@@ -2,14 +2,14 @@
 class Student(object):
     def __init__(self, name, gender):
         self.name = name
-        self.__gender = gender
+        self.gender = gender
     
     def set_gender(self, gender):
         #if self.__gender == 'F' or 'female' or 'Female':
-        self.__gender = gender
+        self.gender = gender
     
     def get_gender(self):
-        if self.__gender == 'F' or 'female' or 'Female' or '女':
+        if self.gender == 'F' or 'female' or 'Female' or '女':
             print('女')
     
     def get_name(self):
@@ -18,10 +18,16 @@ class Student(object):
 class Lisa(Student):
     def __init__(self):
         self.name = 'Lisa'
-        self.__gender = 'Female'
+        self.gender = 'Female'
+    
+    def get_gender(self):
+        print('Lisa\'s gender is female')
+    
 
 if __name__ == '__main__':
     lisa = Lisa()
     lisa.get_name()
     # lisa.set_gender('F')
-    # lisa.get_gender()
+    lisa.get_gender()
+    print(isinstance(lisa, Lisa))
+    print(type(lisa))
