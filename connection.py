41524d4a36bc48d1387555e1de91f80943385039
莫育良 <moyl@signalway.com.cn>
2020-10-26 16:49:27 +0800

    html practice 2

--- connection.py
+++ connection.py
@@ -3,8 +3,8 @@ import socket
 
 # new a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('www.sina.com.cn', 80))
-s.send(b'GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection: close\r\n\r\n')
+s.connect(('203.195.133.195', 80))
+s.send(b'GET / HTTP/1.1\r\nHost: http://203.195.133.195/epgp\r\nConnection: close\r\n\r\n')
 buffer = []
 while True:
     d = s.recv(1024)
@@ -17,5 +17,5 @@ s.close()
 header, html = data.split(b'\r\n\r\n', 1)
 print(header.decode('utf-8'))
 #print(buffer)
-with open('sina.html', 'wb') as f:
+with open('epgp.html', 'wb') as f:
     f.write(html)
